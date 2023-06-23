498f6831dd1d37b36e5cd53c54002f504186a5b3
LAPTOP-UAM396NM\JimGerace <wwc_063571@126.com>
2023-06-23 17:05:06 +0800

    修改this指向

--- utils/tools.ts
+++ utils/tools.ts
@@ -1,10 +1,11 @@
 export const throttle = (fn: Function, wait: number) => {
     let flag = false
+    const _this = this
     return function(...args: any[]){
         if(!flag){
             flag = true
             setTimeout(() => {
-                fn.apply(this, args)
+                fn.apply(_this, args)
                 flag = false
             }, wait)
         }
