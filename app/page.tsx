a4c39783045236209f0f91fbc7465e1e2d1bcb4a
LAPTOP-UAM396NM\JimGerace <wwc_063571@126.com>
2023-07-29 12:58:48 +0800

    更新时效性

--- app/page.tsx
+++ app/page.tsx
@@ -1,22 +1,23 @@
-import NavLeft from "@/components/NavLeft"
-import NavHeader from "@/components/NavHeader"
-import NavRight from "@/components/NavRight"
-import GoBack from "@/components/GoBack"
-import './page.scss'
-import getNavLinks from "./link"
+import NavLeft from "@/components/NavLeft";
+import NavHeader from "@/components/NavHeader";
+import NavRight from "@/components/NavRight";
+import GoBack from "@/components/GoBack";
+import "./page.scss";
+import getNavLinks from "./link";
 
+export const revalidate = 60;
 
-export default async function IndexPage(){
-  const navResource = await getNavLinks()
-  const navResult = navResource.map(item => {
+export default async function IndexPage() {
+  const navResource = await getNavLinks();
+  const navResult = navResource.map((item) => {
     return {
       id: item.id,
       icon: item.icon,
-      title: item.title
-    }
-  })
+      title: item.title,
+    };
+  });
   return (
-    <div className='main_page'>
+    <div className="main_page">
       <GoBack />
       <div className="main_left">
         <NavLeft navResult={navResult} />
@@ -27,5 +28,5 @@ export default async function IndexPage(){
         <NavRight navResource={navResource} />
       </div>
     </div>
-  )
+  );
 }
